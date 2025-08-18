e7eace59fbc2b5e06711be7de5f7d7185c564e1e
skniro <46392259+skniro@users.noreply.github.com>
2025-08-18 18:24:03 +0800

    Function:shears with dispensers in order to collect the apples.  Fix: The age of the crop will not return to 0 after harvesting.  Adjust: Make LeafCropBlock.fruitItem public.

--- Forge/src/main/java/com/skniro/golden_apple_tree/GoldenAppleTree.java
+++ Forge/src/main/java/com/skniro/golden_apple_tree/GoldenAppleTree.java
@@ -2,6 +2,7 @@ package com.skniro.golden_apple_tree;
 
 import com.mojang.logging.LogUtils;
 import com.skniro.golden_apple_tree.block.GoldenAppleTreeBlocks;
+import com.skniro.golden_apple_tree.event.GoldenAppleDispenserBehaviors;
 import com.skniro.golden_apple_tree.item.AppleItems;
 import com.skniro.golden_apple_tree.item.ModCreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
@@ -36,6 +37,7 @@ public class GoldenAppleTree {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
+        GoldenAppleDispenserBehaviors.register();
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
