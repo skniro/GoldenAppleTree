0070d5636623c31d6d63bf224f2ceab4da56b01b
skniro <46392259+skniro@users.noreply.github.com>
2025-10-02 20:00:31 +0800

    Re-add neoforge jade support.

--- Neoforge/src/main/java/com/skniro/golden_apple_tree/compat/jade/GoldenAppleWailaPlugin.java
+++ Neoforge/src/main/java/com/skniro/golden_apple_tree/compat/jade/GoldenAppleWailaPlugin.java
@@ -1,13 +1,13 @@
 package com.skniro.golden_apple_tree.compat.jade;
 
 import com.skniro.golden_apple_tree.block.init.LeafCropBlock;
-/*import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
-import snownee.jade.api.IWailaPlugin;*/
-//import snownee.jade.api.WailaPlugin;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
 
-//@WailaPlugin
-public class GoldenAppleWailaPlugin{ /*implements IWailaPlugin {
+@WailaPlugin
+public class GoldenAppleWailaPlugin implements IWailaPlugin {
 
   @Override
   public void register(IWailaCommonRegistration registration) {
@@ -17,5 +17,5 @@ public class GoldenAppleWailaPlugin{ /*implements IWailaPlugin {
   @Override
   public void registerClient(IWailaClientRegistration registration) {
     registration.registerBlockComponent(LeafCropBlockProgressProvider.INSTANCE, LeafCropBlock.class);
-  }*/
+  }
 }
