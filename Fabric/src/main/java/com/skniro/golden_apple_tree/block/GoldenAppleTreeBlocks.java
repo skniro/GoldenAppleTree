d566d7c78e46db4d93f33b4f305f21d6151ec07a
skniro <46392259+skniro@users.noreply.github.com>
2026-01-14 21:01:48 +0800

    Fix the leaves break instantly.

--- Fabric/src/main/java/com/skniro/golden_apple_tree/block/GoldenAppleTreeBlocks.java
+++ Fabric/src/main/java/com/skniro/golden_apple_tree/block/GoldenAppleTreeBlocks.java
@@ -6,6 +6,7 @@ import com.skniro.golden_apple_tree.world.Tree.AppleSaplingGenerator;
 import com.skniro.golden_apple_tree.world.Tree.EnchantedGoldenAppleSaplingGenerator;
 import com.skniro.golden_apple_tree.world.Tree.GoldenAppleSaplingGenerator;
 import net.minecraft.block.*;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
@@ -13,9 +14,11 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public class GoldenAppleTreeBlocks {
@@ -29,13 +32,13 @@ public class GoldenAppleTreeBlocks {
 
     //LEAVES
     public static final Block Golden_APPLE_LEAVES =registerBlock("golden_apple_leave",
-            (settings)-> new LeafCropBlock(settings, Items.GOLDEN_APPLE), AbstractBlock.Settings.create().nonOpaque().mapColor(MapColor.DARK_RED));
+            (settings)-> new LeafCropBlock(settings, Items.GOLDEN_APPLE), AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).strength(0.2F).sounds(BlockSoundGroup.GRASS).nonOpaque().suffocates(Blocks::never).blockVision(Blocks::never).burnable().pistonBehavior(PistonBehavior.DESTROY).solidBlock(Blocks::never));
     public static final Block ENCHANTED_GOLDEN_APPLE_LEAVES =registerBlock("enchanted_golden_apple_leave",
-            (settings)-> new LeafCropBlock(settings, Items.ENCHANTED_GOLDEN_APPLE), AbstractBlock.Settings.create().nonOpaque().mapColor(MapColor.DARK_RED));
+            (settings)-> new LeafCropBlock(settings, Items.ENCHANTED_GOLDEN_APPLE), AbstractBlock.Settings.create().nonOpaque().mapColor(MapColor.DARK_GREEN).strength(0.2F).sounds(BlockSoundGroup.GRASS).nonOpaque().suffocates(Blocks::never).blockVision(Blocks::never).burnable().pistonBehavior(PistonBehavior.DESTROY).solidBlock(Blocks::never));
     public static final Block APPLE_LEAVES =registerBlock("apple_leave",
-            (settings)-> new LeafCropBlock(settings, Items.APPLE), AbstractBlock.Settings.create().nonOpaque().mapColor(MapColor.DARK_RED));
+            (settings)-> new LeafCropBlock(settings, Items.APPLE), AbstractBlock.Settings.create().nonOpaque().mapColor(MapColor.DARK_GREEN).strength(0.2F).sounds(BlockSoundGroup.GRASS).nonOpaque().suffocates(Blocks::never).blockVision(Blocks::never).burnable().pistonBehavior(PistonBehavior.DESTROY).solidBlock(Blocks::never));
     public static final Block Apple_Tree_LEAVES =registerBlock("apple_tree_leave",
-            (properties)-> new TintedParticleLeavesBlock(0.01F, properties), AbstractBlock.Settings.create().nonOpaque() .mapColor(MapColor.DARK_RED));
+            (properties)-> new TintedParticleLeavesBlock(0.01F, properties), AbstractBlock.Settings.create().nonOpaque().mapColor(MapColor.DARK_GREEN).strength(0.2F).sounds(BlockSoundGroup.GRASS).nonOpaque().suffocates(Blocks::never).blockVision(Blocks::never).burnable().pistonBehavior(PistonBehavior.DESTROY).solidBlock(Blocks::never));
 
     //Potted Plant
     public static final Block POTTED_Golden_APPLE_SAPLING = registerBlockWithoutItem("potted_haste_apple_sapling",
--- Forge/src/main/java/com/skniro/golden_apple_tree/block/GoldenAppleTreeBlocks.java
+++ Forge/src/main/java/com/skniro/golden_apple_tree/block/GoldenAppleTreeBlocks.java
@@ -6,15 +6,19 @@ import com.skniro.golden_apple_tree.item.AppleItems;
 import com.skniro.golden_apple_tree.world.Tree.AppleSaplingGenerator;
 import com.skniro.golden_apple_tree.world.Tree.EnchantedGoldenAppleSaplingGenerator;
 import com.skniro.golden_apple_tree.world.Tree.GoldenAppleSaplingGenerator;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -36,13 +40,13 @@ public class GoldenAppleTreeBlocks {
 
     //LEAVES
     public static final RegistryObject<Block> Golden_APPLE_LEAVES =registerBlock("golden_apple_leave",
-            (settings)-> new LeafCropBlock(settings, Items.GOLDEN_APPLE), BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.NETHER));
+            (settings)-> new LeafCropBlock(settings, Items.GOLDEN_APPLE), BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).strength(0.2F).sound(SoundType.GRASS).noOcclusion().isSuffocating(GoldenAppleTreeBlocks::never).isViewBlocking(GoldenAppleTreeBlocks::never).ignitedByLava().pushReaction(PushReaction.DESTROY).isRedstoneConductor(GoldenAppleTreeBlocks::never));
     public static final RegistryObject<Block> ENCHANTED_GOLDEN_APPLE_LEAVES =registerBlock("enchanted_golden_apple_leave",
-            (settings)-> new LeafCropBlock(settings, Items.ENCHANTED_GOLDEN_APPLE), BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.NETHER));
+            (settings)-> new LeafCropBlock(settings, Items.ENCHANTED_GOLDEN_APPLE), BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).strength(0.2F).sound(SoundType.GRASS).noOcclusion().isSuffocating(GoldenAppleTreeBlocks::never).isViewBlocking(GoldenAppleTreeBlocks::never).ignitedByLava().pushReaction(PushReaction.DESTROY).isRedstoneConductor(GoldenAppleTreeBlocks::never));
     public static final RegistryObject<Block> APPLE_LEAVES =registerBlock("apple_leave",
-            (settings)-> new LeafCropBlock(settings, Items.APPLE), BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.NETHER));
+            (settings)-> new LeafCropBlock(settings, Items.APPLE), BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.PLANT).strength(0.2F).sound(SoundType.GRASS).noOcclusion().isSuffocating(GoldenAppleTreeBlocks::never).isViewBlocking(GoldenAppleTreeBlocks::never).ignitedByLava().pushReaction(PushReaction.DESTROY).isRedstoneConductor(GoldenAppleTreeBlocks::never));
     public static final RegistryObject<Block> Apple_Tree_LEAVES =registerBlock("apple_tree_leave",
-            (properties)-> new TintedParticleLeavesBlock(0.01F, properties), BlockBehaviour.Properties.of().noOcclusion() .mapColor(MapColor.NETHER));
+            (properties)-> new TintedParticleLeavesBlock(0.01F, properties), BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.PLANT).strength(0.2F).sound(SoundType.GRASS).noOcclusion().isSuffocating(GoldenAppleTreeBlocks::never).isViewBlocking(GoldenAppleTreeBlocks::never).ignitedByLava().pushReaction(PushReaction.DESTROY).isRedstoneConductor(GoldenAppleTreeBlocks::never));
 
     //Potted Plant
     public static final RegistryObject<Block> POTTED_Golden_APPLE_SAPLING = registerBlockWithoutItem("potted_haste_apple_sapling",
@@ -72,6 +76,10 @@ public class GoldenAppleTreeBlocks {
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, Supplier<T> block) {
         return AppleItems.ITEMS.register(name, () -> new BlockItem(block.get(),
                 new Item.Properties().useBlockDescriptionPrefix().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(GoldenAppleTree.MOD_ID, name)))));
+    }
+
+    private static boolean never(BlockState state, BlockGetter getter, BlockPos pos) {
+        return false;
     }
 
     public static void registerGoldenAppleTreeBlocks(BusGroup eventBus) {
